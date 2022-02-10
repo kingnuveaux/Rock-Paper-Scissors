a501700f7996e266097de8a9de57cdaa4ee5d2f5
kingnuveaux <terrancemoore23@gmail.com>
2022-02-09 23:47:58 -0500

    Java update

---
+++ java.js
@@ -0,0 +1,75 @@
+function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    let randomChoices = choices[Math.floor(Math.random() * choices.length)];
+ 
+    return randomChoices    
+}
+
+function playRound(playerSelection, computerSelection) {
+    //Your code here!
+    if (playerSelection.toLowerCase() == "rock" & computerSelection == "rock") {
+        message = "Tie!"
+    }
+
+    else if (playerSelection.toLowerCase() == "paper" & computerSelection == "paper") {
+        message = "Tie!"
+    }
+
+    else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "scissors") {
+        message = "Tie!"
+    }
+
+    else if (playerSelection.toLowerCase() == "rock" & computerSelection == "paper") {
+        message = "You lose! Paper beats Rock!"
+    }
+
+    else if (playerSelection.toLowerCase() == "rock" & computerSelection == "scissors") {
+        message = "You win! Rock beats Scissors!"
+    }
+
+    else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "paper") {
+        message = "You win! Scissors beats Paper!"
+    }
+
+    else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "rock") {
+        message = "You lose! Rock beats Scissors!"
+    }
+
+    else if (playerSelection.toLowerCase() == "paper" & computerSelection == "rock") {
+        message = "You win! Paper beats Rock!"
+    }
+
+    else if (playerSelection.toLowerCase() == "paper" & computerSelection == "scissors") {
+        message = "You lose! Scissors beats Paper!"
+    }
+
+    else message = 'Try again using "Rock, Paper or Scissors".'
+
+    return message
+}
+
+var playerSelection = "rock";
+var computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+        playerSelection = window.prompt("Rock, Paper or Scissors?", "What you got?");
+        
+        //if (playerSelection != ("rock" || "paper" || "scissors")) {
+        //    alert("Please enter rock, paper or scissors only! This is your last warning!!");
+        //    i--
+        //    if (i < 0) {
+        //        i = 0
+        //    }
+        //}
+        console.log(playRound (playerSelection, computerSelection));
+    }
+    
+}
+
+console.log(game());
+
+//  GAME NEEDS COMPUTER TO CHOOSE DIFFERENT RESULT EACH RECURSION
+//  GAME DOESN'T RECOGNIZE INPUT AFTER 1ST GUESS (CAN INPUT ANYTHING!) GAME NEEDS TO RECOGNIZE NEW INPUT EACH RECURSION
