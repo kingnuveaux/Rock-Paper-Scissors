0faa62a7a69e15ab585edf61da8027dfd139b0a4
kingnuveaux <terrancemoore23@gmail.com>
2022-02-23 03:58:35 -0500

    Updated game with buttons

--- java.js
+++ java.js
@@ -1,7 +1,29 @@
+const rock = document.getElementById("1");
+const paper = document.getElementById("2")
+const scissors = document.getElementById("3")
+
+// The Game Start
+function game() {
+    let playerWin = 0;
+    let computerWin = 0;
+    let gameWinner = "";
+
+    //Add event listeners for all three buttons/run round on click/ track and end game
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.className;
+            const computerSelection = computerPlay();
+            battleWinText.textContent = playRound(playerSelection, computerSelection);
+            playerWinText.textContent = "Player win totals :" + playerWin;
+            computerWinText.textContent = "Computer win totals: " + computerWin;
+            endGame();
+        })
+    })   
+
 function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
     let randomChoices = choices[Math.floor(Math.random() * choices.length)];
- 
     return randomChoices    
 }
 
@@ -21,56 +43,111 @@ function playRound(playerSelection, computerSelection) {
 
     else if (playerSelection.toLowerCase() == "rock" & computerSelection == "paper") {
         message = "You lose! Paper beats Rock!"
+        computerWin++
     }
 
     else if (playerSelection.toLowerCase() == "rock" & computerSelection == "scissors") {
         message = "You win! Rock beats Scissors!"
+        playerWin++
     }
 
     else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "paper") {
         message = "You win! Scissors beats Paper!"
+        playerWin++
     }
 
     else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "rock") {
         message = "You lose! Rock beats Scissors!"
+        computerWin++
     }
 
     else if (playerSelection.toLowerCase() == "paper" & computerSelection == "rock") {
         message = "You win! Paper beats Rock!"
+        playerWin++
     }
 
     else if (playerSelection.toLowerCase() == "paper" & computerSelection == "scissors") {
         message = "You lose! Scissors beats Paper!"
+        computerWin++
     }
 
     else message = 'Try again using "Rock, Paper or Scissors".'
 
     return message
 }
+//Create div Dom for all results
 
-var playerSelection = "rock";
-//console.log(playRound(playerSelection, computerSelection));
+    const container = document.querySelector("#container");
+    const resultsDiv = document.createElement("div");
+    resultsDiv.style.marginTop = "20 px";
+    container.appendChild(resultsDiv);
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        // your code here!
-        playerSelection = window.prompt("Rock, Paper or Scissors?", "What you got?");
-        
-        var computerSelection = computerPlay();
-        //if (playerSelection != ("rock" || "paper" || "scissors")) {
-        //    alert("Please enter rock, paper or scissors only! This is your last warning!!");
-        //    i--
-        //    if (i < 0) {
-        //        i = 0
-        //    }
-        //}
-        results = playRound (playerSelection, computerSelection)
-        console.log(results)
-    }
+
+//Create computer win tracking DOM
+    const playerWinText = document.createElement("p");
+    playerWinText.style.color = "blue";
+    playerWinText.textContent = "Player win totals: " + playerWin;
+    resultsDiv.appendChild(playerWinText);
+
+//Create computer win tracking DOM
+    const computerWinText = document.createElement("p");
+    computerWinText.style.color = "blue";
+    computerWinText.textContent = "Computer win totals: " + computerWin;
+    resultsDiv.appendChild(computerWinText);
+
+//Create battle win text DOM
+    const battleWinText = document.createElement ("p");
+    battleWinText.style.color = "black";
+    resultsDiv.appendChild(battleWinText);
     
+//Create game win text DOM
+    const gameWinText = document.createElement("p");
+    gameWinText.style.color = "orange";
+    gameWinText.textContent = gameWinner;
+    resultsDiv.appendChild(gameWinText);
+
+//Determine who won to five points first
+    function endGame() {
+        if (playerWin == 5) {
+            gameWinner = "YOU WIN! ";
+            gameWinText.textContent = gameWinner;
+        
+        //disable game buttons
+            document.getElementById("1").disabled = true;
+            document.getElementById("2").disabled = true;
+            document.getElementById("3").disabled = true;
+        
+        //create new DOM button to replay
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Play Again!";
+            resultsDiv.appendChild(playAgainButton);
+
+        //if clicked, reload page
+        playAgainButton.addEventListener('click', () => {
+            location.reload();
+            })
+
+        } else if (computerWin == 5) {
+            gameWinner = "COMPUTER WINS!";
+            gameWinText.textContent = gameWinner;
+
+        //disable game buttons
+            document.getElementById("1").disabled = true;
+            document.getElementById("2").disabled = true;
+            document.getElementById("3").disabled = true;
+
+        //create new DOM button to replay
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Play Again!";
+            resultsDiv.appendChild(playAgainButton);
+
+        //if clicked, reload page
+            playAgainButton.addEventListener('click', () => {
+            location.reload();
+            })
+        }
+    }
 }
 
-console.log(game());
-
-//  GAME NEEDS COMPUTER TO CHOOSE DIFFERENT RESULT EACH RECURSION
-//  GAME DOESN'T RECOGNIZE INPUT AFTER 1ST GUESS (CAN INPUT ANYTHING!) GAME NEEDS TO RECOGNIZE NEW INPUT EACH RECURSION
+//fucntion call to start the game
+game()
